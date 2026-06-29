8b3e2f5acd3d278804aaa84440741c471ed7267d
jaydzm <jaydzm@163.com>
2026-06-30 00:15:14 +0800

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
         <div className="flex flex-col items-center justify-center min-h-[50vh] text-center">
           <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
           <p className="mb-8">
-            Sorry, the page you are looking for does not exist.
+            抱歉,这里什么都没有.
           </p>
           <Button asChild className="not-prose mt-6">
             <Link href="/">Return Home</Link>

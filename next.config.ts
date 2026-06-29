c0341f494a645d055831eb0d2f7cd9d9f447f03d
jaydzm <jaydzm@163.com>
2026-06-30 00:02:43 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: toppuer.top,
+        hostname: WORDPRESS_HOSTNAME,
         port: "",
         pathname: "/**",
       },
